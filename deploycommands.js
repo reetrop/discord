70cc7dcc4be374d63793a8db001ec6193b33d60a
reetrop <bigwhitething101@outlook.com>
2025-11-17 21:55:33 -0600

    Add deploycommands.js for registering Discord commands

---
+++ deploycommands.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const fs = require("fs");
+const { REST, Routes } = require("discord.js");
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log("Done!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
